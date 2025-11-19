a662bfba0d9f33a1dfe439987f8a242dd0d4137a
Aymen Zahed <aymenzahed62@gmail.com>
2025-11-19 18:41:55 +0100

    chore: add JWT authentication package
    - Install djangorestframework-simplejwt package
    - Add JWT settings to Django configuration
    - Update INSTALLED_APPS and REST_FRAMEWORK settings

--- backend/user_service/user_service/settings.py
+++ backend/user_service/user_service/settings.py
@@ -39,9 +39,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt'
     'rest_framework',
     'users',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
